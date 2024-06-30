25644edc71580f52e5ba3eb867350657237c5eaf
Stacy Knapp <franksmom@MacBook-Air.localdomain>
2024-06-29 19:27:48 -0500

    finished hurray

--- candidate-testing.js
+++ candidate-testing.js
@@ -3,37 +3,61 @@ const input = require('readline-sync');
 // TODO 2: modify your quiz app to ask 5 questions //
 
 // TODO 1.1a: Define candidateName // 
-let candidateName;
-// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
-let question;
-let correctAnswer;
-let candidateAnswer;
+let candidateName = "";
 
+// TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+let question = "Who was the first American woman in space? ";
+let correctAnswer = "Sally Ride";
+let candidateAnswer = "";
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = [
+  'Who was the first American woman in space? ',
+  'True or false: 5 kilometer == 5000 meters? ',
+  '(5 + 3)/2 * 10 = ? ',
+  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+  'What is the minimum crew size for the ISS? '
+];
+let correctAnswers = [
+  'Sally Ride',
+  'true',
+  '40',
+  'Trajectory',
+  '3'
+];
+let candidateAnswers = [];
 
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName = input.question(`\nPlease enter your name:\n`);
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+//candidateAnswer = input.question(question);
+for (let i = 0;i < questions.length; i++){
+  candidateAnswers[i] = input.question(questions[i]);
+}
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
 
+  let numberCorrect = 0;
+  for (let i = 0;i < correctAnswers.length; i++){
+    console.log(`Your answer: ${candidateAnswers[i]} Correct answer: ${correctAnswers[i]} `);
+  if (candidateAnswers[i].toUpperCase() == correctAnswers[i].toUpperCase()){
+    console.log("Correct");
+    numberCorrect = numberCorrect + 1;
+  } else {
+    console.log("Incorrect");
+  }
+  }
 
+  let grade = (numberCorrect / questions.length) * 100;  //TODO 3.2 use this variable to calculate the candidates score.
 
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
 
 
   return grade;
